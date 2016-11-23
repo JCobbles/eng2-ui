2d6719f189ef9c30192b2a4edc16193c3979e3d2
艺萱 崔 <yixuancui@eduroam-int-dhcp-97-12-134.ucl.ac.uk>
2016-11-23 12:12:18 +0000

    test

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('Challenge 2');
+  res.send('Challenge 2000');
 });
 
 app.listen(3000, function () {
